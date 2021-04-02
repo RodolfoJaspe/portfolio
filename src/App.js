8edddce538a8bf580207edecb2b6e1c36281c77b
RodolfoJaspe <macosi@Rodolfos-iMac.local>
2021-04-01 20:08:27 -0400

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Route, NavLink, Switch} from "react-router-dom";
 import './App.css';
+import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>sup dawg</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="App">
+            <div className="App-body">
+                <header className="header">
+                    <h1>Rodolfo Jaspe</h1>
+                    <div className="header-nav">
+                        <NavLink to="/">Home</NavLink>
+                        <NavLink to= "/projects">Projects</NavLink>
+                        <NavLink to="/about">About Me</NavLink>
+                    </div>
+               
+                </header>
+                <main>
+                    <Switch>
+                        <Route exact path="/" />
+                        <Route path="/projects" component={Projects}/>
+                        <Route path="/about" component={About} />
+                    </Switch>
+                </main>
+            </div>
+        </div>
   );
 }
 
---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Projects () {
+    return (
+        <div>
+            <h3>Projects</h3>
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
