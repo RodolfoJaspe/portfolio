ce7be610fc465b7c926f1007e4728e6e5c9ced04
Rodolfo Jaspe <68761286+RodolfoJaspe@users.noreply.github.com>
2023-01-23 17:23:16 -0500

    added loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import { Canvas } from '@react-three/fiber';
 import React, { Suspense, useEffect } from 'react';
+import { Html, useProgress } from '@react-three/drei'
 import './App.css';
 import About from './components/About';
 import { Earth } from './components/earth';
 import Header from './components/Header';
 import Projects from './components/Projects';
+
+function Loader() {
+    const { progress } = useProgress()
+    return <Html center><div style={{fontSize:'2rem', color:"#ffffffde", width:"100vw", height:"100vh", display:"flex", alignItems: 'flex-end'}}>{progress} % loaded</div></Html>
+}
 
 function App() {
 
@@ -17,7 +23,7 @@ function App() {
             </div>
             <div className='globe'>
                <Canvas>
-                <Suspense fallback={null}>
+                <Suspense fallback={<Loader/>}>
                     <Earth />
                 </Suspense>
                 </Canvas> 
